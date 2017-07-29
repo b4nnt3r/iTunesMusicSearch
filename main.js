9d9fdc2e7fdaa0d91624e4ea6da45ede7ddac568
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-28 21:01:54 -0400

    holy git I almost forgot to commit

--- main.js
+++ main.js
@@ -1,9 +1,60 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
+let container = document.querySelector(".container")
+let button = document.querySelector(".search_button");
+let input = document.querySelector(".search_song");
+let searchResults = document.querySelector(".results");
+let player = document.querySelector(".music_player")
+let audioSource = document.querySelector(".audioSource");
 
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
+container.addEventListener("click", function(e) {
+  let inputValue = input.value;
+  console.log("e is: ", e)
+
+  if (e.target === button) {
+
+    fetch(`https://itunes.apple.com/search?term=${inputValue}`).then(
+
+        function(response) {
+
+          if (response.status !== 200) {
+            console.log(response.status);
+            return;
+          }
+
+          response.json().then(function(obj) {
+
+            let results = obj.results;
+
+            console.log(results.forEach(function(track) {
+              console.log(track);
+
+              let albumCover = track.artworkUrl100
+              var sample = track.previewUrl
+              let artist = track.artistName
+              let songTitle = track.trackName
+              console.log(artist)
+
+
+              let tracks = `<div class="wrapper">
+                              <div class="sampleSrc" src="${sample}"></div>
+                                <a href="#" src="${sample}"><button class="albumBtn" name="button" >
+                                <img class="image" value="${sample}" src="${albumCover}" alt="album_cover"></button></a>
+                                <div id="title"><a href="#" src="${sample}">${songTitle}</a>
+                              </div>
+                            </div>`
+
+              searchResults.innerHTML += tracks;
+
+            }));
+          });
+        })
+      .catch(function(err) {
+        console.log("fetch error :-S", err);
+      });
+  }
+
+  if (e.target && e.target.matches("img.image")) {
+    audioSource.src = e.target.getAttribute('value');
+    player.load();
+    player.play();
+  }
+})
